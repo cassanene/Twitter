a988e1f420ef182739b9a70e2969ede373e14ee7
Cassey Anene <cassanene@fb.com>
2019-07-03 15:42:15 -0700

    06//03/19 3:41pm

---
+++ twitter/View Controllers/ComposeViewController.m
@@ -0,0 +1,46 @@
+//
+//  ComposeViewController.m
+//  twitter
+//
+//  Created by cassanene on 7/3/19.
+//  Copyright © 2019 Emerson Malca. All rights reserved.
+//
+
+#import "ComposeViewController.h"
+
+@interface ComposeViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *composeTextView;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *closeButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *tweetButton;
+
+
+@end
+
+@implementation ComposeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.closeButton.target = self;
+    self.closeButton.action = @selector(barButtonCustomPressed:);
+
+    // Do any additional setup after loading the view.
+}
+-(IBAction)barButtonCustomPressed:(UIBarButtonItem*)btn
+{
+    [self dismissViewControllerAnimated:true completion:nil];
+
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
